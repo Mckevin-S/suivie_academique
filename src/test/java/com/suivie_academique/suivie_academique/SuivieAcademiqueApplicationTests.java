89fcd8a3df97d8427d122192de56ca0462c56ff0
Mckevin-S <kamguemmckevin@gmail.com>
2025-12-18 16:09:24 +0100

    Sur le profil test

--- src/test/java/com/suivie_academique/suivie_academique/SuivieAcademiqueApplicationTests.java
+++ src/test/java/com/suivie_academique/suivie_academique/SuivieAcademiqueApplicationTests.java
@@ -2,8 +2,10 @@ package com.suivie_academique.suivie_academique;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SuivieAcademiqueApplicationTests {
 
 	@Test
